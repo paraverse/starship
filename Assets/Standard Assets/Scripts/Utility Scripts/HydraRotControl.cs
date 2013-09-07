3ce3561aa36b98dacd15cc036094b7ffc3d7458c
Dylan Swiggett <dylanswiggett@gmail.com>
2013-09-07 11:48:31 -0400

    Fixes hydra, and makes globe look better

---
+++ Assets/Standard Assets/Scripts/Utility Scripts/HydraRotControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HydraRotControl : MonoBehaviour {
+	
+	GameObject handL;
+	GameObject handR;
+	
+	Vector3 lPrev;
+	Vector3 rPrev;
+	
+	// Use this for initialization
+	void Start () {
+		handL = GameObject.Find("Left Hand");
+		handR = GameObject.Find("Right Hand");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*
+		Material mat = (Material) gameObject.GetComponent<Material>();
+		Vector3 l = handL.transform.position;
+		Vector3 r = handR.transform.position;
+		
+		mat.color.r = (l - lPrev).magnitude;
+		*/
+	}
+}
