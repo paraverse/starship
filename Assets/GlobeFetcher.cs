346e899b6c15a1aa5ac021145e3551a2b9cc684c
Lane Aasen <laneaasen@gmail.com>
2013-09-07 12:03:32 -0400

    fixes issue with point placement on the globe

--- Assets/GlobeFetcher.cs
+++ Assets/GlobeFetcher.cs
@@ -14,19 +14,18 @@ public class GlobeFetcher : MonoBehaviour {
 		foreach (var jsonObject in j.list) {
 			float r = gameObject.transform.localScale.x;
 			
-			float latitude = float.Parse(jsonObject.GetField("coordinates").GetField("latitude").ToString());
-			float longitude = float.Parse(jsonObject.GetField("coordinates").GetField("longitude").ToString());			
+			float latitude = float.Parse(jsonObject.GetField("coordinates").GetField("latitude").ToString()) * ((float) System.Math.PI / 180f);
+			float longitude = float.Parse(jsonObject.GetField("coordinates").GetField("longitude").ToString()) * ((float) System.Math.PI / 180f);			
 			float magnitude = float.Parse(jsonObject.GetField("magnitude").ToString());
 			
-			float x = (float) (r * System.Math.Cos(latitude) * System.Math.Cos(longitude));
-			float y = (float) (r * System.Math.Cos(latitude) * System.Math.Sin(longitude));
-			float z = (float) (r * System.Math.Sin(latitude));
+			float x = (float) -(r * System.Math.Cos(latitude) * System.Math.Sin(longitude));
+			float y = (float) (r * System.Math.Sin(latitude));
+			float z = (float) (r * System.Math.Cos(latitude) * System.Math.Cos(longitude));
 			
 			GameObject bar = GameObject.CreatePrimitive(PrimitiveType.Cube);
         	
 			bar.transform.position = gameObject.transform.position + (new Vector3(x, y, z) * 0.5f);
-			
-			bar.transform.localScale = new Vector3(0.05f, 0.05f, magnitude * 20f);
+			bar.transform.localScale = new Vector3(0.05f, 0.05f, magnitude * 10f);
 			bar.transform.LookAt(gameObject.transform);
 			
 		}
