9149502998d2f6a49a7a033ea8c8807bef7403ae
Dylan Swiggett <dylanswiggett@gmail.com>
2013-09-07 15:02:46 -0400

    Adds better earth visuals, more(ish) control

---
+++ Assets/GlobeFetcher.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class GlobeFetcher : MonoBehaviour {
+	
+	public string url = "http://dev.aasen.in:1337/github-globe/";
+	
+	List<GameObject> lines;
+	List<Vector3> positions;
+	List<float> mags;
+    
+	IEnumerator Start() {
+		lines = new List<GameObject>();
+		positions = new List<Vector3>();
+		mags = new List<float>();
+		
+        WWW www = new WWW(url);
+        yield return www;
+        
+		JSONObject j = new JSONObject(www.text);
+		
+		foreach (var jsonObject in j.list) {
+			float r = gameObject.transform.localScale.x;
+			
+			float latitude = float.Parse(jsonObject.GetField("coordinates").GetField("latitude").ToString()) * ((float) System.Math.PI / 180f);
+			float longitude = float.Parse(jsonObject.GetField("coordinates").GetField("longitude").ToString()) * ((float) System.Math.PI / 180f);			
+			float magnitude = float.Parse(jsonObject.GetField("magnitude").ToString());
+			
+			mags.Add(magnitude);
+			
+			float x = (float) -(r * System.Math.Cos(latitude) * System.Math.Sin(longitude));
+			float y = (float) (r * System.Math.Sin(latitude));
+			float z = (float) (r * System.Math.Cos(latitude) * System.Math.Cos(longitude));
+			
+			Vector3 relativePos = new Vector3(x, y, z) * .5f;
+			Vector3 pos = gameObject.transform.position + relativePos;
+			Vector3 dir = relativePos.normalized;
+			
+			GameObject center = new GameObject();
+			center.transform.position = gameObject.transform.position;
+			
+			LineRenderer line = (LineRenderer) center.AddComponent(typeof(LineRenderer));
+			line.SetVertexCount(2);
+			line.SetPosition(0, pos);
+			line.SetPosition(1, pos + dir * magnitude * 10);
+			line.SetWidth(.3f, .3f);
+			
+			line.material = new Material (Shader.Find("Particles/Additive"));
+			Color color = new Color(magnitude * 2, 0, 1-magnitude);
+			line.SetColors(color, color);
+			
+			lines.Add(center);
+			positions.Add(relativePos);
+			
+			/*
+			GameObject bar = GameObject.CreatePrimitive(PrimitiveType.Cube); 
+			
+			bar.transform.position = gameObject.transform.position + (new Vector3(x, y, z) * 0.5f);
+			
+			bar.transform.localScale = new Vector3(0.05f, 0.05f, magnitude * 20f);
+			bar.transform.LookAt(gameObject.transform);
+			*/
+		}
+	}
+	
+	void Update() {
+		for (int i = 0; i < lines.Count; i++) {
+			
+			Vector3 relativePos = positions[i];
+			relativePos = gameObject.transform.rotation * relativePos;
+			Vector3 pos = gameObject.transform.position + relativePos;
+			Vector3 dir = relativePos.normalized;
+			
+			LineRenderer line = (LineRenderer) lines[i].GetComponent("LineRenderer");
+			line.transform.position = gameObject.transform.position;
+			line.SetVertexCount(2);
+			line.SetPosition(0, pos);
+			line.SetPosition(1, pos + dir * mags[i] * 10);
+			
+		}
+	}
+}
--- Assets/Standard Assets/Scripts/Utility Scripts/HydraRotControl.cs
+++ Assets/Standard Assets/Scripts/Utility Scripts/HydraRotControl.cs
@@ -9,20 +9,43 @@ public class HydraRotControl : MonoBehaviour {
 	Vector3 lPrev;
 	Vector3 rPrev;
 	
+	Vector3 rotSpeed;
+	
 	// Use this for initialization
 	void Start () {
 		handL = GameObject.Find("Left Hand");
 		handR = GameObject.Find("Right Hand");
+		rotSpeed = new Vector3(0, 0, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		/*
-		Material mat = (Material) gameObject.GetComponent<Material>();
+		if (handL == null)
+			handL = GameObject.Find("Left Hand");
+		if (handR == null)
+			handR = GameObject.Find("Right Hand");
+		
+		// Material mat = (Material) gameObject.GetComponent<Material>();
 		Vector3 l = handL.transform.position;
 		Vector3 r = handR.transform.position;
 		
-		mat.color.r = (l - lPrev).magnitude;
-		*/
+		bool move = false;
+		
+		if (gameObject.collider.bounds.Contains(handL.transform.position) != gameObject.collider.bounds.Contains (handR.transform.position)) {
+			move = true;
+			Vector3 v1 = gameObject.transform.rotation * (l - lPrev);
+			Vector3 v2 = gameObject.transform.rotation * (l - gameObject.transform.position);
+			rotSpeed = Vector3.Cross(v1, v2);
+		}
+		
+		gameObject.renderer.material.color = new Color(1, 1 - rotSpeed.magnitude / 5,
+			1 - rotSpeed.magnitude / 5, gameObject.renderer.material.color.a);
+		
+		lPrev = l;
+		rPrev = r;
+		
+		gameObject.transform.Rotate(rotSpeed);
+		rotSpeed = rotSpeed * .7f;
+		//rotSpeed = new Vector3(0, 1, 0);
 	}
 }
