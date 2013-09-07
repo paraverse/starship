51a69322b907c821796bb0428aa1cf2af2b632fe
Lane Aasen <laneaasen@gmail.com>
2013-09-07 01:38:27 -0400

    adds github globe integration. pretty cool

---
+++ Assets/GlobeFetcher.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobeFetcher : MonoBehaviour {
+	
+	public string url = "http://dev.aasen.in:1337/github-globe/";
+    
+	IEnumerator Start() {
+        WWW www = new WWW(url);
+        yield return www;
+        
+		JSONObject j = new JSONObject(www.text);
+	
+		foreach (var jsonObject in j.list) {
+			float r = gameObject.transform.localScale.x;
+			
+			float latitude = float.Parse(jsonObject.GetField("coordinates").GetField("latitude").ToString());
+			float longitude = float.Parse(jsonObject.GetField("coordinates").GetField("longitude").ToString());			
+			float magnitude = float.Parse(jsonObject.GetField("magnitude").ToString());
+			
+			float x = (float) (r * System.Math.Cos(latitude) * System.Math.Cos(longitude));
+			float y = (float) (r * System.Math.Cos(latitude) * System.Math.Sin(longitude));
+			float z = (float) (r * System.Math.Sin(latitude));
+			
+			GameObject bar = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        	
+			bar.transform.position = gameObject.transform.position + (new Vector3(x, y, z) * 0.5f);
+			
+			bar.transform.localScale = new Vector3(0.05f, 0.05f, magnitude * 20f);
+			bar.transform.LookAt(gameObject.transform);
+			
+		}
+	}
+}
